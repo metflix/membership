075209fb1db2c13d9d7d65efc626ebb97dadac32
making <makingx@gmail.com>
2016-05-18 02:57:48 +0900

    Modify script

--- ci/tasks/merge-to-master.sh
+++ ci/tasks/merge-to-master.sh
@@ -5,10 +5,9 @@ git config --global user.name "Toshiaki Maki"
 cd out
 shopt -s dotglob
 mv -f ../repo-master/* ./
+git remote add -f develop ../repo
 git status
 git branch -a
 git log master | head -n 20 | cat
 git log develop/develop | head -n 20 | cat
-git remote add -f develop ../repo
 git merge --no-edit develop/develop
-git status
