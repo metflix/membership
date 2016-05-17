d93765255978133c39ed0a501c4138cf5e7bf04b
making <makingx@gmail.com>
2016-05-18 02:52:04 +0900

    Fix pipeline

--- ci/tasks/merge-to-master.sh
+++ ci/tasks/merge-to-master.sh
@@ -5,6 +5,10 @@ git config --global user.name "Toshiaki Maki"
 cd out
 shopt -s dotglob
 mv -f ../repo-master/* ./
+git status
+git branch -a
+git log master | head -n 20 | cat
+git log develop/develop | head -n 20 | cat
 git remote add -f develop ../repo
 git merge --no-edit develop/develop
 git status
